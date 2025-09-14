65621eea108a258b6b5ff701453961ed31cdbae6
Karn Wong <karn@karnwong.me>
2025-09-14 14:24:52 +0700

    call foo_nodes.foo

--- src/resonate_benchmark/main.py
+++ src/resonate_benchmark/main.py
@@ -1,5 +1,20 @@
+import os
+from uuid import uuid4
+
+from resonate import Resonate
+
+resonate = Resonate.remote(host=os.environ["RESONATE_HOST"])
+
+
 def main():
-    print("Hello from resonate-benchmark!")
+    try:
+        result = resonate.options(target="poll://any@foo_nodes").rpc(
+            id=str(uuid4()), func="foo", v="baz"
+        )
+
+        print(result)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
